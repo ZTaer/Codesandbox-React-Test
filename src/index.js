faa9b0576c3aedbadabe3270cb38d2d72e112765
ztaerkiller <37570712+ZTaer@users.noreply.github.com>
2019-12-16 02:38:45 +0800

    这是一个测试

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "bootstrap/dist/css/bootstrap.css";
 import "./styles.css";
 import "./test.scss";
 
 function App() {
   return (
     <div className="App">
-      <h1>tewst</h1>
+      <h1 className="display-1">这编辑器牛皮</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
